83815f8b5ec049731afe21cfea21e25dad855209
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-05-24 20:54:44 +0530

    update some codes

--- day_1.py
+++ day_1.py
@@ -29,22 +29,14 @@ def get_pivot_point(arr, left,right):
     return border
 
 def find_element(arr, left, right, k):
-    # Check whether k is still inside the partition
-    if k > 0 and k <= (right - left + 1):
-        # get the pivot
-        pivot_index = get_pivot_point(arr, left, right)
-        
-        # If k happens to be on the pivot
-        if (pivot_index - left == k - 1): 
-            return arr[pivot_index]
-        
-        # if k is on the right side of pivot, apply the solution
-        # recursively to the right partition
-        if (pivot_index - left > k - 1):   
-            return find_element(arr, left, pivot_index - 1, k) 
-        
-        # else left side and update both k index and left index
-        return find_element(arr, (pivot_index + 1), right, (k - pivot_index + left - 1)) 
+    pivot_point = get_pivot_point(arr, left, right)
+    
+    if pivot_point == k - 1:
+        return arr[pivot_point]
+    elif pivot_point < k - 1:
+        return find_element(arr, pivot_point + 1, right, k)
+    else:
+        return find_element(arr, left, pivot_point - 1, k)
         
 
 def kth_smallest_element(arr, k):
